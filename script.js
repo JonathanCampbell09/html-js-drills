ebd8e40f33114dedaf723b91d00626304ff84788
JonathanCampbell09 <85598289+JonathanCampbell09@users.noreply.github.com>
2021-07-02 01:47:14 -0400

    more work done to finish up drills

--- script.js
+++ script.js
@@ -58,9 +58,9 @@ function replaceImage() {
 
 function changeCodeStatus() {
   console.log('Called changeCodeStatus()');
-  var meme = document.querySelector('#codestatus');
+  let codestatus = document.querySelector('#codestatus');
   codestatus.innerHTML =""
-  var meme = document.createElement("img");
+  let meme = document.createElement("img");
   meme.setAttribute("src", "gunbran.png");
   meme.setAttribute("width", "300");
   meme.setAttribute("height", "300");
@@ -111,11 +111,31 @@ informationForm.addEventListener('submit', function(event) {
   document.getElementById('chosencar').innerHTML = formProps.cars;
   document.getElementById('icecreamstatus').innerHTML = formProps.icecream;
   
-
-  let humancheck= document.querySelector('#humancheck').checked;
-  let codercheck= document.querySelector('#codercheck').checked;
-  document.querySelector('#checks').innerHTML = `${humanCheck} ${codercheck}`;
-
+  let iceyes = document.getElementById("icecreamyes");
+  let iceno= document.getElementById("icecreamno");
+  if (iceyes.checked){
+    document.getElementById("icecreamstatus").textContent =iceyes.value;
+  }
+  else if(iceno.checked){
+    document.getElementById("icecreamstatus").textContent =iceno.value;
+  }
+  let humancheck = document.getElementById("humancheck");
+  let codercheck = document.getElementById("codercheck");
+  if(humancheck.checked && codercheck.checked){
+    document.getElementById("checks").textContent =null;
+    let li = document.createElement('li');
+    li.textContent = humancheck.labels[0].innerText;
+    document.getElementById("checks").appendChild(li);
+    li = document.createElement('li');
+    li.textContent = codercheck.labels[0].innerText;
+    document.getElementById("checks").appendChild(li);
+  }
+  else if(codercheck.checked){
+    document.getElementById("checks").textContent = codercheck.labels[0].innerText;
+  }
+  else if(humancheck.checked){
+    document.getElementById("checks").textContent = humancheck.labels[0].innerText;
+  }
   // Your job:
   //   1. Get information typed into the form
   //   2. Display that info in "Display that info here" section
@@ -137,15 +157,17 @@ informationForm.addEventListener('submit', function(event) {
 // then log something to the console
 
 // Fill in ________ to get a reference to the correct button on the page
-let consoleLogButton = document.querySelector('#todos');
+let consoleLogButton = document.querySelector('#console-log-button');
 
 // Log something when that button is clicked
 consoleLogButton.addEventListener('click', function() {
-  console.log('Change this text if you want!');
+  console.log('I changed this text :)');
 });
 
-let makeBlueButton = document.querySelector('#________');
+let makeBlueButton = document.querySelector('#make-blue-button');
 makeBlueButton.addEventListener('click', function() {
+
+document.getElementById("colorText").style.color = 'blue';
   // Your job:
   //  1. When a user clicks "Change the text to the right blue"
   //  2. Change the text in <div id="colorText">...</div> to blue
@@ -153,7 +175,7 @@ makeBlueButton.addEventListener('click', function() {
 
 // Adding an event listener to document means the "keydown" event
 // can happen anywhere on the page and we'll respond.
-document.addeventListener('keydown', function() {
+
   // This is called whenever a user pressed any key.
 
   // Your job:
@@ -163,6 +185,16 @@ document.addeventListener('keydown', function() {
   // See:
   // - https://developer.mozilla.org/en-US/docs/Web/API/Document/keydown_event
   // - https://javascript.info/keyboard-events
+  addEventListener("keydown", function(event) {
+    if (event.key == 'r')
+    document.getElementById("colorText").style.color = 'red';
+
+});
+  addEventListener("keydown", function(event) {
+    if (event.key == 'g')
+    document.getElementById("colorText").style.color = 'green';
+
+
 });
 
 /*************************************
@@ -180,8 +212,20 @@ document.addeventListener('keydown', function() {
  *   it is next to.
  */
 
-// Your code goes here
+ let submit = document.querySelector(".form");
+ let lists = document.getElementById("todos");
+ let box = document.querySelector(".title");
+ let buttons=[];
+ submit.addEventListener('submit',function(event){
+   
+   event.preventDefault();
+   
+   let text = document.querySelector("#todo");
+   let itemList = document.createElement('li');
+   itemList.textContent = text.value;
+   lists.appendChild(itemList);
 
+});
 /****************************************
  * Section 5 - setInterval + setTimeout *
  ****************************************
@@ -199,7 +243,27 @@ document.addeventListener('keydown', function() {
  *
  */
 
-// Your code goes here
+ let j =1;
+ let counter = document.querySelector("#seconds");
+ function stopWatch(){
+   
+   let textj = document.createTextNode(j);
+ 
+  counter.parentNode.insertBefore(textj,counter);
+  setTimeout(function(){counter.parentNode.removeChild(textj);},900);
+   j++;
+ }
+ setInterval(function() {stopWatch();},1000);
+ 
+ let display = document.querySelector("#delayedDisplay");
+ let div = document.createElement("div");
+ let img = document.createElement("img");
+ img.src = "";
+ img.className = meme;
+ div.appendChild(img);
+ display.parentNode.insertBefore(div,display);
+ 
+ setTimeout(function(){img.src="gunbran.jpg";},5000);
 
 /****************************************
  * Section 6 - Your own section!        *
@@ -213,4 +277,4 @@ document.addeventListener('keydown', function() {
  *
  */
 
-// Your code goes here
+  // Your code goes here
